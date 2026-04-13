3d83edaf12b65d70451bb47b4596c8182f4644fc
joao.cordeiro <joao.cordeiro@smartgreen.net>
2026-04-13 12:38:36 -0300

    feat(qt): menu "Configurações" abre tela de configurações

--- lib/poffin_qt.py
+++ lib/poffin_qt.py
@@ -1,5 +1,41 @@
-from PyQt6.QtWidgets import QMainWindow  # , QApplication
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtCore import QThread
+from PyQt6.QtGui import QAction
 from ui.mainwindow import Ui_MainWindow
+from ui.configwindow import Ui_ConfigWindow
+
+
+class Worker(QThread):
+    """TODO
+    """
+    def __init__(self, func, args):
+        """TODO
+        """
+
+        super(Worker, self).__init__()
+        self.func = func
+        self.args = args
+
+    def run(self):
+        """TODO
+        """
+
+        self.func(*self.args)
+
+    def set_args(self, args):
+        """TODO
+        """
+
+        self.args = args
+
+
+class PoffinConfigQt(QMainWindow, Ui_ConfigWindow):
+    def __init__(self):
+        """Poffin Config Window Class
+        """
+
+        super(PoffinConfigQt, self).__init__()
+        self.setupUi(self)
 
 
 class PoffinQt(QMainWindow, Ui_MainWindow):
@@ -9,3 +45,8 @@ class PoffinQt(QMainWindow, Ui_MainWindow):
 
         super(PoffinQt, self).__init__()
         self.setupUi(self)
+
+        self.config_window = PoffinConfigQt()
+        self.menuSettings.triggered.connect(
+            self.config_window.show
+        )
